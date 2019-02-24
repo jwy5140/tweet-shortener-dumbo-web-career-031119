58ea4c18e1e67a35d6f018d1e52ce3906191d2b4
Jeffrey Yu <jwy5140@gmail.com>
2019-02-24 14:38:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  {
+    ["hello"] => "hi", ["to", "two", "too"] => "2", ["for", "four"] => "4", ["be"] => "b", ["you"] => "u", ["at"] => "@", ["and"] => "&"
+  }
+end
+
+def word_substituter(str)
+  arr = str.split(" ")
+  n = 0 
+  while n < arr.length do
+    dictionary.each {|x| 
+      x.each {|x,y| binding.pry
+        if x.include?(str)
+          return y 
+        end
+      }
+    }
+    n += 1
+  end
+
+end
+
+def bulk_tweet_shortener(arr)
+end
+
+def selective_tweet_shortener(arr)
+end
