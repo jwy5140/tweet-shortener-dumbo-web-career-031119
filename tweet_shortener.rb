08d79b789f9a0f301319fe5514ffe78942444a67
Jeffrey Yu <jwy5140@gmail.com>
2019-02-24 14:38:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,19 +10,29 @@ def word_substituter(str)
   arr = str.split(" ")
   n = 0 
   while n < arr.length do
-    dictionary.each {|x| 
-      x.each {|x,y| binding.pry
-        if x.include?(str)
-          return y 
+    dictionary.each {|x,y|
+      if x.include?(arr[n].downcase)
+        if arr[n].capitalize == arr[n]
+          arr[n] = y.capitalize
+        elsif arr[n].upcase == arr
+          arr[n] = y.upcase
+        else 
+          arr[n] = y 
         end
-      }
+      end
     }
     n += 1
   end
-
+  arr.join(" ")
 end
 
 def bulk_tweet_shortener(arr)
+  n = 0 
+  while n < arr.length do 
+    arr[n] = word_substituter(arr[n])
+    puts arr[n]
+    n += 1 
+  end
 end
 
 def selective_tweet_shortener(arr)
