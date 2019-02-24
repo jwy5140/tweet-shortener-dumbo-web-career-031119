de9e0cf6a129c752d624ea4f7ea7639eaeef337e
Jeffrey Yu <jwy5140@gmail.com>
2019-02-24 14:48:38 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,53 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  {
+    ["hello"] => "hi", ["to", "two", "too"] => "2", ["for", "four"] => "4", ["be"] => "b", ["you"] => "u", ["at"] => "@", ["and"] => "&"
+  }
+end
+
+def word_substituter(str)
+  arr = str.split(" ")
+  n = 0 
+  while n < arr.length do
+    dictionary.each {|x,y|
+      if x.include?(arr[n].downcase)
+        if arr[n].capitalize == arr[n]
+          arr[n] = y.capitalize
+        elsif arr[n].upcase == arr
+          arr[n] = y.upcase
+        else 
+          arr[n] = y 
+        end
+      end
+    }
+    n += 1
+  end
+  arr.join(" ")
+end
+
+def bulk_tweet_shortener(arr)
+  n = 0 
+  while n < arr.length do 
+    arr[n] = word_substituter(arr[n])
+    puts arr[n]
+    n += 1 
+  end
+end
+
+def selective_tweet_shortener(str)
+  if str.length > 140
+    return word_substituter(str)
+  end
+  str
+end
+
+def shortened_tweet_truncator(str)
+  if str.length > 140
+    str = word_substituter(str)
+    if str.length > 140
+      str = "#{str[0...137]}..."
+    end
+  end
+  str
+end
